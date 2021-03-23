70d610a6fa99f7112bb46ff505add96c0b7eaef7
John R Foster <fosterj@bu.edu>
2021-03-23 08:59:35 -0400

    add met and move inits to model file

--- 04.5_groupEffects.R
+++ 04.5_groupEffects.R
@@ -24,10 +24,16 @@ year.effect <- FALSE
 # n.cores <- as.numeric(Sys.getenv("NSLOTS")) # number of cores to use for cluster 
 n.cores <- 3
 
-source("Models/yearTimeSeriesBasic.R")
+source("Models/yearTimeSeriesMetDiff.R")
+group.dir <- "singleObsError"
 
-ForecastProject.id <- "yearTimeSeriesBasic"     # Some ID that applies to a set of forecasts
-out.dir <- here("ModelOut", ForecastProject.id)
+# for getting met variable
+csv <- "Data/BioTemperatureWeekly.csv"
+met.var <- "bioTempMaximum"
+met.uncertainty <- "bioTempMaximumVariance"
+
+ForecastProject.id <- "SppSiteSurv"     # Some ID that applies to a set of forecasts
+out.dir <- here("ModelOut", group.dir)
 if(!dir.exists(out.dir)) dir.create(out.dir)
 
 # split data by species
@@ -71,26 +77,56 @@ ids <- colnames(wide.data[-c(1, 2)])
 species.vec <- c(rep("ixodes", length(plots.ixodes)), rep("amblyomma", length(plots.amblyomma)))
 plot.vec <- stringr::str_extract(ids, "[[:upper:]]{4}_\\d{3}")
 site.vec <- stringr::str_extract(ids, "[[:upper:]]{4}")
+year.weeks <- tibble(yearWeek = wide.data$yearWeek)
 
-# convert groupings to numeric vectors
-year.vec <- year(wide.data$time) - min(year(wide.data$time)) + 1
-species.vec <- as.factor(species.vec) %>% as.numeric()
-plot.vec <- as.factor(plot.vec) %>% as.numeric()
-site.vec <- as.factor(site.vec) %>% as.numeric()
+# convert groupings to numeric vectors for indexing
+year.index <- year(wide.data$time) - min(year(wide.data$time)) + 1
+species.index <- as.factor(species.vec) %>% as.numeric()
+plot.index <- as.factor(plot.vec) %>% as.numeric()
+site.index <- as.factor(site.vec) %>% as.numeric()
 
 # number of each group
-n.species <- length(unique(species.vec))
-n.sites <- length(unique(site.vec))
-n.years <- length(unique(year.vec))
+n.species <- length(unique(species.index))
+n.sites <- length(unique(site.index))
+n.years <- length(unique(year.index))
 years <- year(wide.data$time) %>% unique()
 
 # put data in an array by year
 y.table <- wide.data[,-c(1,2)]
-weeks.per.year <- year.vec %>% table()
+weeks.per.year <- year.index %>% table()
 max.weeks <-  max(weeks.per.year)
 y <- array(NA, dim = c(3, # years
                        max.weeks, # weeks
                        ncol(y.table))) # site
+
+
+source("Functions/get_met_array.R")
+met.list <- get_met_array(csv, 
+                          weeks.per.year,
+                          year.weeks,
+                          met.var,
+                          met.uncertainty)
+met.vals <- met.list$met.vals
+x.var <- met.list$met.uncertainty
+met.inits <- met.list$met.inits
+
+# pull out driver variance, check for NAs, set to mean
+if(any(is.na(x.var))){
+  x.var[is.na(x.var)] <- mean(x.var, na.rm = TRUE)
+}
+x.var[which(x.var == 0)] <- 0.1
+
+# scaling met by standard deviation by site
+met.inits <- met.inits / sd(met.inits, na.rm = TRUE)
+for(ss in 1:n.sites){
+  site.sd <- sd(met.vals[,,ss], na.rm = TRUE)  
+  met.vals[,,ss] <- met.vals[,,ss] / site.sd
+  
+  # variance is scaled by the square of the scale (sd)
+  x.var[,,ss] <- x.var[,,ss] / site.sd^2
+}
+
+
 
 for(i in 1:3){
   y.sub <- wide.data %>% 
@@ -99,7 +135,12 @@ for(i in 1:3){
   y[i, 1:weeks.per.year[i], 1:ncol(y.table)] <- as.matrix(y.sub[,-c(1,2)]) 
 }
 
-data <- list(y = y)
+data <- list(
+  y = y,
+  life.constraint = array(1, dim = dim(y)),
+  x.obs = met.vals,
+  x.tau = 1 / x.var
+)
 
 mu.inits <- y
 for(i in 1:3){
@@ -113,43 +154,34 @@ mu.inits[is.na(mu.inits)] <- 0
 
 total.mu.index <- dim(mu.inits)[1]*dim(mu.inits)[2]*dim(mu.inits)[3]
 
-inits <- function(){list(
-  rho = rnorm(1, 0, 10),
-  y = mu.inits + runif(total.mu.index, 0, 5),
-  z = mu.inits + runif(total.mu.index, 0, 5),
-  x = mu.inits + runif(total.mu.index, 0, 5),
-  ex = mu.inits + runif(total.mu.index, 0, 5),
-  tau.process = runif(1, 0, 0.1), 
-  # tau.site = runif(1, 0, 0.1),
-  tau.obs = runif(2, 0, 0.1)
-  # alpha.species = rnorm(n.species, 0, 1),
-  # alpha.site = rnorm(n.sites, 0, 1),
-  # alpha.year = rnorm(n.years, 0, 1),
-  # tau.species = runif(1, 0, 0.1)
-)}
-
-monitor <- c("tau.process", "tau.obs", "rho", "x")
-if(species.effect) monitor <- c(monitor, "tau.species", "alpha.species")
-if(site.effect) monitor <- c(monitor, "tau.site", "alpha.site")
-if(year.effect) monitor <- c(monitor, "tau.year", "alpha.year")
-
 x.ic <- matrix(runif(n.years*ncol(y), 0, 1), n.years, ncol(y))
-
 
 constants <- list(
   n.weeks = weeks.per.year,
   n.plots = ncol(y.table),
   x.ic = x.ic, #runif(ncol(y), 0, 1),
-  n.species = n.species,
+  met.mu = 0,
+  tau.met = 1 / var(met.list$met.vals, na.rm = TRUE),
   n.sites = n.sites,
+  n.species = n.species,
+  species = species.index,
   n.years = n.years,
-  species = species.vec,
-  site = site.vec,
-  year = year.vec,
-  species.effect = species.effect,
-  site.effect = site.effect,
-  year.effect = year.effect
+  site = site.index
 )
+
+# monitor originally defined in model script sourced at top
+if(site.effect) monitor <- c(monitor, "tau.site", "alpha.site")
+if(species.effect){
+  monitor <- c(monitor, "tau.species", "alpha.species")
+  constants$n.species <- n.species
+  constants$species <- species.index
+} 
+if(year.effect){
+  monitor <- c(monitor, "tau.year", "alpha.year")
+  constants$year <- year.index
+} 
+
+
 
 model.rw <- nimbleModel(model, 
                         constants = constants,
@@ -158,6 +190,7 @@ model.rw <- nimbleModel(model,
 model.rw$initializeInfo()
 cModel.rw <- compileNimble(model.rw)
 mcmcConf <- configureMCMC(cModel.rw, monitors = monitor)
+# mcmcConf$addSampler(target = c("tau.obs[1]", "tau.obs[2]"), type = "RW_block")
 mcmcBuild <- buildMCMC(mcmcConf)
 mcmcBuild$run(1)
 
@@ -165,14 +198,16 @@ mcmc.start <- Sys.time()
 message(paste("Start time:", mcmc.start))
 
 file.name <- ForecastProject.id
-if(species.effect) file.name <- paste0(file.name, "Species")
-if(site.effect) file.name <- paste0(file.name, "Site")
-if(year.effect) file.name <- paste0(file.name, "Year")
+if(species.effect) file.name <- paste0(file.name, "SpeciesEffect")
+if(site.effect) file.name <- paste0(file.name, "SiteEffect")
+if(year.effect) file.name <- paste0(file.name, "YearEffect")
 rdata.name <- paste0(file.name, ".RData")
 
 save.path <- here("ModelOut",
-                  ForecastProject.id,
+                  group.dir,
                   rdata.name)
+
+message(paste0("Writing model output to ", save.path))
 
 source("Functions/run_nimble_parallel.R")
 cl <- makeCluster(n.cores)
@@ -183,8 +218,8 @@ samples <- run_nimble_parallel(
     data,
     inits,
     monitor,
-    n.iter = 50000,
-    max.iter = 2e6,
+    n.iter = 30000,
+    max.iter = 5e6,
     thin = 10,
     check.interval = 5,
     check.params.only = TRUE,
