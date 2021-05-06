20e136757aa110316cfd49e19e0e1b809fd51086
John R Foster <fosterj@bu.edu>
2021-05-06 09:43:05 -0400

    update bash

--- RunModel.sh
+++ RunModel.sh
@@ -2,11 +2,24 @@
 #$ -j y
 #$ -m ea
 #$ -l buyin
-#$ -l h_rt=12:00:00
-#$ -pe omp 4
-#$ -l mem_per_core=16G
-#$ -N nullNimbleTest
+#$ -l h_rt=72:00:00
+#$ -pe omp 8
+#$ -l mem_per_core=18G
 #$ -o ../Output_Files/RCNfx/
 
 module load R/4.0.2
-Rscript 03_RandomWalk.R
+
+# #$ -N plotEffects
+# Rscript 04.1_plotEffects.R
+
+# #$ -N yearEffects
+# Rscript 04.2_yearEffects.R
+
+# #$ -N cumGDDDiff
+# Rscript 04.3_indModelSetUp.R
+
+# #$ -N smam
+# Rscript 04.4_indSmamPrevYear.R
+
+#$ -N obsPoisTempSurv
+Rscript 04.5.1_groupEffectsPois.R
