033907d0fa12fd0c0da758d83b33f52742405253
JohnRFoster <fosterj@bu.edu>
2021-06-29 14:48:01 -0400

    model variables in data list to use posteriors

--- Models/yearTimeSeriesCGDDPois.R
+++ Models/yearTimeSeriesCGDDPois.R
@@ -15,10 +15,13 @@ model <- nimbleCode({
   theta ~ dbeta(1, 10) # transition / death
   
   beta ~ dnorm(beta.mu, tau = beta.tau) # intercept; observation model
-  psi ~ dnorm(0, tau = 0.1) # intercept; survival
-  tau.beta.site ~ dgamma(shape = tau.beta.site.shape, rate = tau.beta.site.rate)  # across site variability in detection
-  tau.psi.site ~ dgamma(0.5, 1)  # across site variability in survival
-  tau.process ~ dgamma(shape = tau.process.shape, rate = tau.process.rate) # process error
+  psi ~ dnorm(psi.mu, tau = psi.tau) # intercept; survival
+  tau.beta.site ~ dgamma(shape = tau.beta.site.shape, 
+                         rate = tau.beta.site.rate)  # across site variability in detection
+  tau.psi.site ~ dgamma(shape = tau.psi.site.shape, 
+                        rate = tau.psi.site.rate)  # across site variability in survival
+  tau.process ~ dgamma(shape = tau.process.shape, 
+                       rate = tau.process.rate) # process error
 
   
   # data model, density so using dnorm
@@ -45,7 +48,7 @@ model <- nimbleCode({
   # driver data model
   for(s in 1:n.sites){
     beta.site[s] ~ dnorm(mean = beta.site.mu[s], tau = tau.beta.site)
-    psi.site[s] ~ dnorm(0, tau = tau.psi.site)
+    psi.site[s] ~ dnorm(mean = psi.site.mu[s], tau = tau.psi.site)
     for(k in 1:n.years){
       for(t in 1:n.weeks[k]){
         cgdd[k, t, s] ~ dunif(0, 6) # driver prior
@@ -67,8 +70,7 @@ model <- nimbleCode({
       
       for(t in 2:n.weeks[k]){
         ex[k, t, p] <- x[k, t-1, p] + phi[k, t, site[p]]*x[k, t-1, p] - theta*x[k, t-1, p]
-        x[k, t, p] ~ T(dnorm(ex[k, t, p], tau = tau.process), 0, Inf)
-        # z[k, t, p] <- max(0, x[k, t, p])  
+        x[k, t, p] ~ T(dnorm(ex[k, t, p], tau = tau.process), 0, Inf) 
         
       } # weeks
     } # years
@@ -85,23 +87,6 @@ monitor <- c("tau.process",
              "theta",
              "x")
 
-inits <- function(){list(
-  # phi = array(runif(total.mu.index, 0.95, 1), dim = dim(y)),
-  # pi = array(runif(total.mu.index, 0.4, 0.6), dim = dim(y)),
-  theta <- runif(1, 0, 0.1),
-  beta = runif(1, 1, 2),
-  psi = runif(1, 1, 2),
-  beta.site = runif(n.sites, 0.9, 1.1),
-  psi.site = runif(n.sites, 0.9, 1.1),
-  y = round(mu.inits),
-  z = round(mu.inits),# + runif(total.mu.index, 0, 5),
-  x = round(mu.inits),# + runif(total.mu.index, 0, 5),
-  ex = round(mu.inits),# + runif(total.mu.index, 0, 5),
-  ex.z = round(mu.inits),# + runif(total.mu.index, 6, 8),
-  cgdd = met.inits,
-  tau.beta.site = runif(1, 0.3, 0.4),
-  tau.psi.site = runif(1, 0, 0.001),
-  tau.process = runif(1, 0, 1/2000^2) 
-)}
+
 
 
